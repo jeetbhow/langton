7755198282a764fe56b524f01ed9ebd041e47424
Jeet Bhowmik <jeetbhowm@gmail.com>
2025-05-24 15:34:06 -0700

    chore: Write template + documentation for Board class

---
+++ app/src/main/java/model/Board.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Board {
+    int width;
+    int height;
+    Square[] squares;
+
+    /**
+     * Create a Board with the given dimensions. Each square in the board is
+     * by default initialized to black.
+     * @param width The width of the Board.
+     * @param height The height of the Board.
+     */
+    public Board(int width, int height) {
+
+    }
+
+    /**
+     * Flip the state of a particular square on the board.
+     * @param x The x coordinate of the square.
+     * @param y The y coordinate of the square.
+     * @param state The state that you want to set the square to.
+     */
+    public void flipStateAt(int x, int y) {
+
+    }
+
+    /**
+     * Reset the entire state of the board to black.
+     */
+    public void resetState() {
+
+    }
+}
