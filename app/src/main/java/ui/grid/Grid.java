886b06fb523d80dcbe44f1d744b4d337ea1534b0
Jeet Bhowmik <jeetbhowm@gmail.com>
2025-05-28 16:43:59 -0700

    feature: Added camera zoom in/out

--- app/src/main/java/ui/grid/Grid.java
+++ app/src/main/java/ui/grid/Grid.java
@@ -2,63 +2,90 @@ package ui.grid;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseWheelEvent;
 
 import javax.swing.JPanel;
 
 import model.Board;
+import model.Camera;
 import model.Square.SquareColor;
 
 public class Grid extends JPanel {
     private Board board;
-
-    public Grid() {}
+    private Camera camera;
 
     public Grid(Board board) {
         this.board = board;
+        camera = new Camera(board.getWidth(), board.getHeight());
+
+        addMouseWheelListener(new MouseAdapter() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                double zoomAmount = e.getPreciseWheelRotation();
+                camera.zoom(zoomAmount * 8);
+                repaint();
+            }
+        });
     }
 
     public void setBoard(Board board) {
         this.board = board;
+        camera = new Camera(board.getWidth(), board.getHeight());
+        repaint();
     }
 
-@Override
-protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    
-    if (board == null) return;
-    
-    int panelWidth = getWidth();
-    int panelHeight = getHeight();
-    int boardWidth = board.getWidth();
-    int boardHeight = board.getHeight();
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        if (board == null) return;
+        
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+        double camWidth = camera.getWidth();
+        double camHeight = camera.getHeight();
 
-    for (int i = 0; i < boardHeight; i++) {
-        for (int j = 0; j < boardWidth; j++) {
-            int x1 = (j * panelWidth) / boardWidth;
-            int y1 = (i * panelHeight) / boardHeight;
-            int x2 = ((j + 1) * panelWidth) / boardWidth;
-            int y2 = ((i + 1) * panelHeight) / boardHeight;
-            
-            if (board.getColorAt(i, j) == SquareColor.BLACK) {
-                g.setColor(Color.BLACK);
-            } else {
-                g.setColor(Color.WHITE);
+        double camX = camera.getX();
+        double camY = camera.getY();
+
+        for (int i = 0; i < camHeight; i++) {
+            for (int j = 0; j < camWidth; j++) {
+                double boardX = camX + j;
+                double boardY = camY + i;
+
+                if (boardX < 0 || boardX >= board.getWidth() ||
+                        boardY < 0 || boardY >= board.getHeight()) {
+                    continue; 
+                }
+
+                int x1 = (j * panelWidth) / (int) camWidth;
+                int y1 = (i * panelHeight) / (int) camHeight;
+                int x2 = ((j + 1) * panelWidth) / (int) camWidth;
+                int y2 = ((i + 1) * panelHeight) / (int) camHeight;
+                
+                if (board.getColorAt((int) boardY, (int) boardX) == SquareColor.BLACK) {
+                    g.setColor(Color.BLACK);
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(x1, y1, x2 - x1, y2 - y1);
             }
-            g.fillRect(x1, y1, x2 - x1, y2 - y1);
         }
+
+        var ant = board.getAnt();
+        int antRow = ant.getRow();
+        int antCol = ant.getCol();
+
+        int relativeAntRow = antRow - (int) camera.getY();
+        int relativeAntCol = antCol - (int) camera.getX();
+        
+        int antX1 = (relativeAntCol * panelWidth) / (int) camWidth;
+        int antY1 = (relativeAntRow * panelHeight) / (int) camHeight;
+        int antX2 = ((relativeAntCol + 1) * panelWidth) / (int) camWidth;
+        int antY2 = ((relativeAntRow + 1) * panelHeight) / (int) camHeight;
+
+        g.setColor(Color.RED);
+        g.fillRect(antX1, antY1, antX2 - antX1, antY2 - antY1);
     }
-
-    var ant = board.getAnt();
-    int antRow = ant.getRow();
-    int antCol = ant.getCol();
-    
-    // Same calculation for ant position
-    int antX1 = (antCol * panelWidth) / boardWidth;
-    int antY1 = (antRow * panelHeight) / boardHeight;
-    int antX2 = ((antCol + 1) * panelWidth) / boardWidth;
-    int antY2 = ((antRow + 1) * panelHeight) / boardHeight;
-
-    g.setColor(Color.RED);
-    g.fillRect(antX1, antY1, antX2 - antX1, antY2 - antY1);
-}
 }
